85b88c83ef72861c25c987bcdf94eb2b79006fc1
Edgar Avecilla <edgaravecilla1993@gmail.com>
2022-05-03 01:03:18 -0500

    add decrypt and copy function

--- main.js
+++ main.js
@@ -1,5 +1,50 @@
 "use strict";
 
+function replace(ntext) {
+  let text = ntext;
+  let decryptedText = ["ai", "enter", "imes", "ober", "ufat"];
+  decryptedText.map(function (w) {
+    const word = new RegExp(w, "g");
+    switch (w) {
+      case "ai":
+        text = text.replace(word, "a");
+      case "enter":
+        text = text.replace(word, "e");
+      case "imes":
+        text = text.replace(word, "i");
+      case "ober":
+        text = text.replace(word, "o");
+      case "ufat":
+        text = text.replace(word, "u");
+    }
+  });
+  var textreplaced = text;
+  return textreplaced;
+}
+
+function decrypt() {
+  let text = document.getElementById("textarea").value;
+  let textreplaced = "";
+  let upperCase = /[A-Z]/;
+  let accent = /[À-ðò-ÿ]/;
+  if (upperCase.test(text) != true && accent.test(text) != true) {
+    textreplaced = replace(text);
+    document.getElementById("p-result").innerText = textreplaced;
+    document.getElementById("pre-result").style.display = "none";
+    document.getElementById("result").style.display = "block";
+  } else if (upperCase.test(text) === true || accent.test(text) === true) {
+    let styleidAlert = document.getElementById("idAlert").style;
+    styleidAlert.color = "crimson";
+    styleidAlert.fontSize = "14px";
+    setTimeout(() => {
+      styleidAlert.color = "#495057";
+      styleidAlert.fontSize = "12px";
+    }, 1600);
+    document.getElementById("pre-result").style.display = "block";
+    document.getElementById("result").style.display = "none";
+  }
+}
+
 function encrypt() {
   let text = document.getElementById("textarea").value;
   let encryptedtext = "";
@@ -31,18 +76,20 @@ function encrypt() {
       document.getElementById("pre-result").style.display = "none";
       document.getElementById("result").style.display = "block";
     }
-  }
-  else if (upperCase.test(text) === true || accent.test(text) === true) {
+  } else if (upperCase.test(text) === true || accent.test(text) === true) {
     let styleidAlert = document.getElementById("idAlert").style;
-    styleidAlert.color = 'crimson';
-    styleidAlert.fontSize = '14px';
+    styleidAlert.color = "crimson";
+    styleidAlert.fontSize = "14px";
     setTimeout(() => {
-        styleidAlert.color = '#495057';
-        styleidAlert.fontSize = '12px';
+      styleidAlert.color = "#495057";
+      styleidAlert.fontSize = "12px";
     }, 1600);
     document.getElementById("pre-result").style.display = "block";
     document.getElementById("result").style.display = "none";
   }
 }
 
-
+function copy() {
+  document.getElementById("p-result").select()
+  navigator.clipboard.writeText(document.getElementById("p-result").value)
+}
